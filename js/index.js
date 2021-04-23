82a21817dd4a1a5c756bbcaf252b7e3bd2716b28
artyom kabakov <matroskin7@i.ua>
2021-04-23 15:20:02 +0300

    create map and handle click

--- js/index.js
+++ js/index.js
@@ -1,15 +1,28 @@
 class Tile{
-    constructor(x, y, layer, row) {
+    constructor(x, y, layer, root) {
         this.x = x;
         this.y = y;
         this.layer = layer;
         this.type = 0;
-        this.element = document.createElement("div");
-        let cell = row.insertCell();
+        this.element = document.createElement("img");
+        this.element.src=`img/bamboo/${layer+1}.png`
+        if(layer!==0){
+            this.element.style.backgroundColor="#fff";
+        }
+        let cell = document.createElement("div");
+        cell.className="cell";
+        cell.style.top = `${x*51-layer*3}px`;
+        cell.style.left = `${y*40+layer*3+window.innerWidth/2-8*40}px`
+
         if((LAYERS[layer][x+1]&1<<y)===0 && (x+1)===8){
-            cell.rowSpan=2;
+            cell.style.top = `${x*51-layer*3+25}px`;
+        }
+        if(layer===LAYERS.length-1){
+            cell.style.left = `${y*40+layer*3+window.innerWidth/2-8.5*40}px`
         }
         cell.append(this.element);
+        cell.addEventListener("click", this.handle(this));
+        root.append(cell);
     }
 
     getType(){
@@ -19,29 +32,28 @@ class Tile{
     compare(another){
         return (another.getType() === this.type);
     }
+
+    handle(cell){
+        return ()=>{
+            this.cell = cell;
+            console.log(`x:${this.cell.x} y:${this.cell.y} z:${this.cell.layer}`)
+        }
+    }
 }
 
 
-for(let layer = 0; layer<1; layer++){
-    let table = document.createElement("TABLE");
-    table.style.border='#000';
+for(let layer = 0; layer<5; layer++){
+    let root = document.createElement("div");
+    root.className="root";
+    root.style.top=`${layer}px`
+    root.style.left=`${layer*2}px`
+    console.log(LAYERS[layer])
     for (let i = 0; i < 16; i++){
-        let row = table.insertRow();
         for (let j = 0; j < 16; j++){
-            if(i===5){
-                console.log(`l: ${layer} x:${i} y:${j} : ${(LAYERS[layer][i]&1<<j)===1<<j}`)
-            }
-
             if((LAYERS[layer][i]&1<<j)===1<<j){
-                let t = new Tile(i, j, layer, row);
-            }else{
-                if(i!==8 || (LAYERS[layer][i-1]&1<<j)===0) {
-                    let c = row.insertCell()
-                    c.style.backgroundColor = "red";
-                }
-
+                let t = new Tile(i, j, layer, root);
             }
         }
     }
-    document.body.append(table);
+    document.body.append(root);
 }
